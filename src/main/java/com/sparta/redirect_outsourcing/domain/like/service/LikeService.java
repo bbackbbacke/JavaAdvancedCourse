26f397b1d113ddf9354e45d66a13ffbbd2389f20
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-03 16:02:41 +0900

    자신의 리뷰에 좋아요 할 수 없음 처리

--- src/main/java/com/sparta/redirect_outsourcing/domain/like/service/LikeService.java
+++ src/main/java/com/sparta/redirect_outsourcing/domain/like/service/LikeService.java
@@ -1,6 +1,5 @@
 package com.sparta.redirect_outsourcing.domain.like.service;
 
-import com.sparta.redirect_outsourcing.domain.like.dto.LikeResponseDto;
 import com.sparta.redirect_outsourcing.domain.like.entity.Like;
 import com.sparta.redirect_outsourcing.domain.like.repository.LikeAdapter;
 import com.sparta.redirect_outsourcing.domain.review.entity.Review;
@@ -14,18 +13,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class LikeService {
+
     private final LikeAdapter likeAdapter;
     private final ReviewAdapter reviewAdapter;
     private final UserAdapter userAdapter;
 
+    // 좋아요 달기
     @Transactional
     public void addLike(Long userId, Long reviewId) {
         User user = userAdapter.findById(userId);
         Review review = reviewAdapter.findById(reviewId); // 이미 reviewAdapter에서 .orElsethrow처리 함
 
-        // 동일한 리뷰에 여러 번 좋아요를 누를 수 없도록 예외 처리
+        // 자신의 리뷰에 좋아요 할 수 없음
+        if(review.getUser().getId().equals(userId)) {
+            throw new IllegalArgumentException("자신의 리뷰에 좋아요를 할 수 없습니다.");
+        }
+
+        // 동일한 리뷰에 여러 번 좋아요를 누를 수 없도록 예외 처리(같은 게시물에는 사용자당 한 번만 좋아요가 가능)
         if (likeAdapter.existsByUserAndReview(user, review)) {
-            throw new IllegalArgumentException("User has already liked this review");
+            throw new IllegalArgumentException("해당 리뷰에 이미 좋아요를 하셨습니다.");
         }
         Like like = new Like();
         like.setUser(user);
@@ -36,6 +42,7 @@ public class LikeService {
         Like savedLike = likeAdapter.saveLike(like);
     }
 
+    // 좋아요 삭제
     @Transactional
     public void removeLike(Long userId, Long reviewId) {
         Review review = reviewAdapter.findById(reviewId);
@@ -45,8 +52,10 @@ public class LikeService {
         likeAdapter.deleteLike(userId, reviewId);
     }
 
+    // 좋아요 수
     @Transactional(readOnly = true)
     public int countLikes(Long reviewId) {
         return likeAdapter.countLikes(reviewId);
     }
 }
+
